1562b3461028bdd7bb10934e948915c040f3d37e
kumudinimetta-lab <kumudini.metta@gmail.com>
2026-07-31 15:52:59 +0530

    Added application routing

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing.jsx'
+import ResearchProfile from './pages/ResearchProfile.jsx'
+import MissionControl from './pages/MissionControl.jsx'
+import Recommendations from './pages/Recommendations.jsx'
+import GrantDetails from './pages/GrantDetails.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<ResearchProfile />} />
+        <Route path="/mission-control" element={<MissionControl />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/grant-details" element={<GrantDetails />} />
       </Routes>
     </BrowserRouter>
   )
